7a31350c14bc88281ed83f527eeb25c9eadc1f49
praveencloudangles <112871831+praveencloudangles@users.noreply.github.com>
2024-01-05 17:18:52 +0530

    Update datavisualization.py

--- datavisualization.py
+++ datavisualization.py
@@ -24,23 +24,23 @@ def data_vis():
 
     print(column)
 
-    # for i in column:
-    #     fig = px.histogram(data, y=i)
-    #     fig.update_layout(template='plotly_dark')
-    #     fig.update_xaxes(showgrid=False, zeroline=False)
-    #     fig.update_yaxes(showgrid=False, zeroline=False)
-    #     # fig.show()
-    #     fig.write_image(f"{i}_hist.jpg")
-    #     # a.append(fig)
-    # for i in column:
-    #     fig = px.box(data, y=i)
-    #     fig.update_layout(template='plotly_dark')
-    #     #fig.update_layout(plot_bgcolor = "plotly_dark")
-    #     fig.update_xaxes(showgrid=False,zeroline=False)
-    #     fig.update_yaxes(showgrid=False,zeroline=False)
-    #     # fig.show()
-    #     fig.write_image(f"{i}.jpg")
-    #     # a.append(fig)
+    for i in column:
+        fig = px.histogram(data, y=i)
+        fig.update_layout(template='plotly_dark')
+        fig.update_xaxes(showgrid=False, zeroline=False)
+        fig.update_yaxes(showgrid=False, zeroline=False)
+        # fig.show()
+        fig.write_image(f"{i}_hist.jpg")
+        # a.append(fig)
+    for i in column:
+        fig = px.box(data, y=i)
+        fig.update_layout(template='plotly_dark')
+        #fig.update_layout(plot_bgcolor = "plotly_dark")
+        fig.update_xaxes(showgrid=False,zeroline=False)
+        fig.update_yaxes(showgrid=False,zeroline=False)
+        # fig.show()
+        fig.write_image(f"{i}.jpg")
+        # a.append(fig)
 
     columns_to_remove=["loan_condition_cat","term_cat", "recoveries", "interest_payment_cat", "application_type_cat", "annual_inc", "dti", "emp_length_int", "grade_cat", "total_rec_prncp"]
     df=data.drop(columns=columns_to_remove,axis=1)
